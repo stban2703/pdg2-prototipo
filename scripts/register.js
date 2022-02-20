a3dd70b4fefe1422e81973eeb3f6f769bd960571
stban2703 <s.t.ban1401@gmail.com>
2022-02-20 18:06:46 -0500

    Design mixins, variables, placeholder...

--- scripts/register.js
+++ scripts/register.js
@@ -2,16 +2,20 @@ import { signUp } from "./modules/auth.js"
 
 const signUpForm = document.querySelector('.signUpForm')
 
-signUpForm.addEventListener('submit', function(event) {
+signUpForm.addEventListener('submit', function (event) {
     event.preventDefault()
     const name = signUpForm.name.value
     const lastname = signUpForm.lastname.value
     const email = signUpForm.email.value
     const pass = signUpForm.pass.value
     const confirmpass = signUpForm.confirmpass.value
-    if(name != "" && lastname != "" && email != "" && pass != ""){
-        if(pass === confirmpass) {
-            signUp(email, pass, name, lastname)
+    if (name != "" && lastname != "" && email != "" && pass != "") {
+        if (pass === confirmpass) {
+            if (pass.length >= 8) {
+                signUp(email, pass, name, lastname)
+            } else {
+                alert("La contraseña debe tener almenos 8 caracteres")
+            }
         } else {
             alert("Las contraseñas no coinciden")
         }
