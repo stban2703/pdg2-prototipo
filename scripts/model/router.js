54ccfaeb350f9dd757bdd1ea76e82ffb2023331d
stban2703 <s.t.ban1401@gmail.com>
2022-03-03 14:02:43 -0500

    update router scripts filename

--- scripts/model/router.js
+++ scripts/model/router.js
@@ -1,13 +1,29 @@
-const router = new Router(
-    {
-        home: new Layout(new Page("home.html")),
-        notes: new Layout(new Page("notes.html")),
-        createnotes: new Layout(new Page("createnotes.html")),
-        memointro: new Layout(new Page("memointro.html")),
-        meetinglist: new Layout(new Page("meetinglist.html")),
-        meetingdetails: new Layout(new Page("meetingdetails.html")),
-        createmeeting: new Layout(new Page("createmeeting.html")),
-        "#default": new Page("home.html"),
-    },
-    document.querySelector(".page-content")
-);
+class Router {
+    constructor(routes, el) {
+        this.routes = routes;
+        this.el = el;
+        window.onhashchange = this.hashChanged.bind(this);
+        this.hashChanged();
+    }
+
+    async hashChanged(ev) {
+        if (window.location.hash.length > 0) {
+            let pageName = window.location.hash.substring(1);
+            if (pageName.includes("?")) {
+                let fragments = pageName.split("?")
+                this.show(fragments[0])
+            } else {
+                this.show(pageName);
+            }
+        } else if (this.routes["#default"]) {
+            this.show("#default");
+        }
+    }
+
+    async show(pageName) {
+        const page = this.routes[pageName];
+        await page.load();
+        this.el.innerHTML = "";
+        page.show(this.el);
+    }
+}
---
+++ scripts/model/userouter.js
@@ -0,0 +1,13 @@
+const router = new Router(
+    {
+        home: new Layout(new Page("home.html")),
+        notes: new Layout(new Page("notes.html")),
+        createnotes: new Layout(new Page("createnotes.html")),
+        memointro: new Layout(new Page("memointro.html")),
+        meetinglist: new Layout(new Page("meetinglist.html")),
+        meetingdetails: new Layout(new Page("meetingdetails.html")),
+        createmeeting: new Layout(new Page("createmeeting.html")),
+        "#default": new Page("home.html"),
+    },
+    document.querySelector(".page-content")
+);
