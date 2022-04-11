a829d610fd2a68a686d4302a11b1427864a1efa0
stban2703 <s.t.ban1401@gmail.com>
2022-04-10 21:15:36 -0500

    Render radio question

--- scripts/memoquestion.js
+++ scripts/memoquestion.js
@@ -1,4 +1,5 @@
 import { getMemoQuestion } from "./modules/firestore.js"
+import { asteriskToBold } from "./utils/text-format.js"
 
 export async function renderMemoQuestion() {
     const memoQuestionScreen = document.querySelector(".memoquestion-screen")
@@ -13,9 +14,55 @@ export async function renderMemoQuestion() {
         const questionInfo = await getMemoQuestion(period, subjectId, questionId)
         console.log(questionInfo)
 
+        // Titulo de seccion
         const memoQuestionSectionTitle = memoQuestionScreen.querySelector(".memoquestion-screen__title")
-        const memoQuestionSubsectionTitle = memoQuestionScreen.querySelector(".memoquestion-form__title")
         memoQuestionSectionTitle.innerHTML = questionInfo.section
-        memoQuestionSubsectionTitle.innerHTML = questionInfo.subsection
+
+        // Formulario general
+        const memoQuestionForm = memoQuestionScreen.querySelector(".memoquestion-form")
+
+        if (questionInfo.type !== "improveactions") {
+            // Titulo de subseccion
+            const memoQuestionSubsectionTitle = memoQuestionScreen.querySelector(".memoquestion-form__title")
+            memoQuestionSubsectionTitle.innerHTML = questionInfo.subsection
+            memoQuestionSubsectionTitle.classList.remove("hidden")
+
+            // Contenedor de pregunta normal
+            const memoquestionContainerNormal = memoQuestionForm.querySelector(".memoquestion-form__container--normal")
+            memoquestionContainerNormal.classList.remove("hidden")
+
+            // Renderizar pregunta
+            memoquestionContainerNormal.innerHTML = `
+                    <h5 class="memoquestion-form__subtitle">Pregunta ${questionInfo.index}</h5>
+                    <p class="memoquestion-form__question">${asteriskToBold(questionInfo.question)}
+                    </p>
+                `
+            
+            // Contenedor de respuesta
+            const answerContainer = document.createElement('div')
+            answerContainer.className = "memoquestion-form__answer"
+
+            // Agregar tipo de respuesta
+            switch (questionInfo.type) {
+                case "radio":
+                    const radioAnswerQuestion = document.createElement('div')
+                    radioAnswerQuestion.className = "memoquestion-form__radio-checkbox memoquestion-form__radio-checkbox--radio"
+
+                    questionInfo.options.forEach(option => {
+                        const answerOption = document.createElement('label')
+                        answerOption.className = "memo-radio-input"
+                        answerOption.innerHTML = `
+                        <input type="radio" name="${questionId}" value="${option}" required />
+                        ${option}
+                        `
+                        radioAnswerQuestion.appendChild(answerOption)
+                    });
+                    answerContainer.appendChild(radioAnswerQuestion)
+                    break;
+            }
+
+            memoQuestionForm.querySelector(".memoquestion-form__container").appendChild(answerContainer)
+        }
+
     }
 }
--- scripts/utils/text-format.js
+++ scripts/utils/text-format.js
@@ -1,5 +1,5 @@
 export function asteriskToBold(text) {
     var bold = /\*(.*?)\*/gm;
-    var html = text.replace(bold, '<strong>$1</strong>');            
+    var html = text.replace(bold, "<span style='font-weight: 600'>$1</span>");            
     return html;
 }
