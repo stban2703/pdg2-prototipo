a7aa475c5d0279ba72b2c52480f14c60b3638ea1
stban2703 <s.t.ban1401@gmail.com>
2022-04-16 02:19:37 -0500

    Sort by name accomplishment

--- scripts/accomplishment.js
+++ scripts/accomplishment.js
@@ -1,5 +1,6 @@
 import { getAllAnswersBySubjectsAndPeriod, getGroupInfo, getGroupSubjects, getTeacherById, getUserSubjects, updateTeacherAccomplishment } from "./modules/firestore.js"
 import { hideLoader, showLoader } from "./utils/loader.js"
+import { sortByAlphabeticAscending, sortByAlphabeticDescending } from "./utils/sort.js"
 
 
 export async function getInitialAccomplishmentList(userInfo) {
@@ -20,14 +21,11 @@ export async function getInitialAccomplishmentList(userInfo) {
             }
         });
 
-        const completeList = accomplishmentScreenContentList.querySelector(".accomplishment-screen__columnList--complete")
-        const incompleteList = accomplishmentScreenContentList.querySelector(".accomplishment-screen__columnList--incomplete")
-
-        completeList.innerHTML = ``
-        incompleteList.innerHTML = ``
+        const accomplishmentListControls = document.querySelector(".memoselectsubject-screen__controls--accomplishmentControls")
 
         switch (currentRole) {
             case "leader":
+                accomplishmentListControls.alphabetic.classList.remove("hidden")
                 const groupInfo = await getGroupInfo(userInfo.leaderGroupId)
                 const groupSubjects = await getGroupSubjects(viewId)
                 let completeCounter = 0
@@ -38,47 +36,85 @@ export async function getInitialAccomplishmentList(userInfo) {
                     const id = groupInfo.teachers[index];
                     const teacher = await getTeacherById(id)
                     teacherList.push(teacher)
-                    if(teacher.accomplishment >= 100) {
+                    if (teacher.accomplishment >= 100) {
                         completeCounter++
                     } else {
                         incompleteCounter++
                     }
                 }
-
-                teacherList.forEach(teacher => {
-                    let teacherSubjectsNames = ''
-                    teacher.subjects.forEach(id => {
-                        const q = groupSubjects.find(subject => {
-                            return subject.id === id
-                        })
-                        teacherSubjectsNames += `${q.name}<br>`
-                    })
-                    const teacherItem = document.createElement('div')
-                    teacherItem.className = `accomplishment-teacher${teacher.accomplishment === 100 ? ' accomplishment-teacher--secondary' : ''}`
-                    teacherItem.innerHTML = `
-                    <img src="./images/accomplishmentteacher.svg" alt="">
-                    <section class="accomplishment-teacher__info">
-                        <h5 class="accomplishment-teacher__name">${teacher.name.split(" ")[0]} ${teacher.lastname.split(" ")[0]}</h5>
-                        <p class="accomplishment-teacher__subject">${teacherSubjectsNames}</p>
-                    </section>
-                    `
-                    if(teacher.accomplishment === 100) {
-                        completeList.appendChild(teacherItem)
-                    } else {
-                        incompleteList.appendChild(teacherItem)
-                    }
-                })
-
                 const completeNumberContainer = document.querySelector(".accomplishment-counter__number--complete")
                 const incompleteNumberContainer = document.querySelector(".accomplishment-counter__number--incomplete")
                 completeNumberContainer.innerHTML = `${completeCounter}/${teacherList.length}`
                 incompleteNumberContainer.innerHTML = `${incompleteCounter}`
 
+                renderAccomplishmentTeachers(teacherList, groupSubjects)
+                accomplishmentListControls.addEventListener('input', () => {
+                    sortFilterAccomplishmentTeacher(teacherList, accomplishmentListControls.alphabetic, accomplishmentListControls.career, accomplishmentListControls.group, groupSubjects)
+                })
                 break;
         }
-
         hideLoader()
     }
+}
+
+function sortFilterAccomplishmentTeacher(teacherList, alphabeticSort, careerFilter, groupFilter, groupSubjects) {
+    let filterCopy = [...teacherList]
+
+    if (alphabeticSort.value.length > 0) {
+        if (alphabeticSort.value == "ascending") {
+            filterCopy = [...filterCopy].sort(sortByAlphabeticAscending)
+        } else if (alphabeticSort.value = "descending") {
+            filterCopy = [...filterCopy].sort(sortByAlphabeticDescending)
+        }
+    }
+
+    if (careerFilter.value.length > 0) {
+        filterCopy = [...filterCopy].filter(e => {
+            if (e.group == careerFilter.value) {
+                return true
+            }
+        })
+    }
+
+    if (groupFilter.value.length > 0) {
+        filterCopy = [...filterCopy].filter(e => {
+            if (e.group == groupFilter.value) {
+                return true
+            }
+        })
+    }
+    renderAccomplishmentTeachers(filterCopy, groupSubjects)
+}
+
+function renderAccomplishmentTeachers(list, groupSubjects) {
+    const completeList = document.querySelector(".accomplishment-screen__columnList--complete")
+    const incompleteList = document.querySelector(".accomplishment-screen__columnList--incomplete")
+    completeList.innerHTML = ``
+    incompleteList.innerHTML = ``
+    
+    list.forEach(teacher => {
+        let teacherSubjectsNames = ''
+        teacher.subjects.forEach(id => {
+            const q = groupSubjects.find(subject => {
+                return subject.id === id
+            })
+            teacherSubjectsNames += `${q.name}<br>`
+        })
+        const teacherItem = document.createElement('div')
+        teacherItem.className = `accomplishment-teacher${teacher.accomplishment === 100 ? ' accomplishment-teacher--secondary' : ''}`
+        teacherItem.innerHTML = `
+        <img src="./images/accomplishmentteacher.svg" alt="">
+        <section class="accomplishment-teacher__info">
+            <h5 class="accomplishment-teacher__name">${teacher.name.split(" ")[0]} ${teacher.lastname.split(" ")[0]}</h5>
+            <p class="accomplishment-teacher__subject">${teacherSubjectsNames}</p>
+        </section>
+        `
+        if (teacher.accomplishment === 100) {
+            completeList.appendChild(teacherItem)
+        } else {
+            incompleteList.appendChild(teacherItem)
+        }
+    })
 }
 
 export async function submitUserAccomplishment(userSubjects, currentPeriod) {
