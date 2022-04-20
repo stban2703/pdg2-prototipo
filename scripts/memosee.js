f7be88f34d60af15347d245096323f75e23326c3
stban2703 <s.t.ban1401@gmail.com>
2022-04-19 21:34:51 -0500

    Render see memo subjects and filter

--- scripts/memosee.js
+++ scripts/memosee.js
@@ -1,10 +1,11 @@
-import { getCareerSubjects, getDepartmentCareers, getDepartments } from "./modules/firestore.js";
+import { getCareerInfo, getCareerSubjects, getDepartmentCareers, getDepartments } from "./modules/firestore.js";
 import { hideLoader, showLoader } from "./utils/loader.js";
+import { sortByAlphabeticAscending, sortByAlphabeticDescending } from "./utils/sort.js";
 
 export async function renderMemoseeDepartments() {
     const memoseeDepartments = document.querySelector(".memosee-screen__selectList--departments")
 
-    if(memoseeDepartments && window.location.href.includes("#memoseedepartments")) {
+    if (memoseeDepartments && window.location.href.includes("#memoseedepartments")) {
         showLoader()
         const departments = await getDepartments();
         memoseeDepartments.innerHTML = ``
@@ -27,7 +28,7 @@ export async function renderMemoseeDepartments() {
 export async function renderMemoseeCareers() {
     const memoseeCareers = document.querySelector(".memosee-screen__selectList--careers")
 
-    if(memoseeCareers && window.location.href.includes("#memoseecareers")) {
+    if (memoseeCareers && window.location.href.includes("#memoseecareers")) {
         showLoader()
         const departmentId = window.location.hash.split("?")[1]
         const careers = await getDepartmentCareers(departmentId)
@@ -51,11 +52,71 @@ export async function renderMemoseeCareers() {
 
 export async function getInitialMemoseeSubjects() {
     const memoseeCareers = document.querySelector(".memosee-screen__selectList--subjects")
-    
-    if(memoseeCareers && window.location.href.includes("#memoseesubjects")) {
+
+    if (memoseeCareers && window.location.href.includes("#memoseesubjects")) {
+        showLoader()
         const careerId = window.location.hash.split("?")[1]
+        const careerInfo = await getCareerInfo(careerId)
         const subjects = await getCareerSubjects(careerId)
 
-        
+        const subjectsForm = document.querySelector(".memoselectsubject-screen__controls")
+        careerInfo.groups.forEach(group => {
+            const option = document.createElement('option')
+            option.value = group
+            option.innerHTML = group
+            subjectsForm.group.appendChild(option)
+        })
+        renderMemoseeSubjects(subjects)
+        onSortFilterMemoseeSubjectListener(subjects)
     }
+}
+
+function renderMemoseeSubjects(list) {
+    const memoseeCareers = document.querySelector(".memosee-screen__selectList--subjects")
+    memoseeCareers.innerHTML = ``
+    list.forEach(subject => {
+        const subjectItem = document.createElement("div")
+        subjectItem.className = "memo-subject"
+        subjectItem.innerHTML = `
+            <h5 class="memo-subject__title">${subject.name}</h5>
+            <a class="memo-subject__button small-button small-button--secondary" href="#memoseesubjects?${subject.id}">
+                <span>Seleccionar</span>
+            </a>
+        `
+        memoseeCareers.appendChild(subjectItem)
+    })
+}
+
+function onSortFilterMemoseeSubjectListener(subjects) {
+    const memoseeSubjectsControl = document.querySelector(".memoselectsubject-screen__controls--seeMemos")
+
+    if (window.location.href.includes("#memoseesubjects") && memoseeSubjectsControl) {
+        const subjectsSortSelect = memoseeSubjectsControl.alphabetic
+        const subjectsFilterSelect = memoseeSubjectsControl.group
+
+        memoseeSubjectsControl.addEventListener('input', () => {
+            sortFilterMemoseeSubjects(subjects, subjectsSortSelect, subjectsFilterSelect)
+        })
+    }
+}
+
+function sortFilterMemoseeSubjects(subjects, subjectSort, groupFilter) {
+    let filterCopy = [...subjects]
+
+    if (subjectSort.value.length > 0) {
+        if (subjectSort.value == "ascending") {
+            filterCopy = [...filterCopy].sort(sortByAlphabeticAscending)
+        } else if (subjectSort.value = "descending") {
+            filterCopy = [...filterCopy].sort(sortByAlphabeticDescending)
+        }
+    }
+
+    if (groupFilter.value.length > 0) {
+        filterCopy = [...filterCopy].filter(e => {
+            if (e.group == groupFilter.value) {
+                return true
+            }
+        })
+    }
+    renderMemoseeSubjects(filterCopy)
 }
