d954804a2cfbc8b3fdedec74c6e0df2393a8f770
stban2703 <s.t.ban1401@gmail.com>
2022-05-07 17:13:31 -0500

    choose current role

--- scripts/selectrole.js
+++ scripts/selectrole.js
@@ -1,4 +1,4 @@
-import { localUser } from "./utils/ls.js";
+import { localUser, setLocalStorage } from "./utils/ls.js";
 
 const selectRoleContainer = document.querySelector(".signinup-screen__selectRole")
 selectRoleContainer.innerHTML = ""
@@ -9,7 +9,7 @@ userNameContainer.innerHTML = localUser.name
 localUser.role.forEach(role => {
     const roleItem = document.createElement('div')
     roleItem.className = "select-role"
-
+    roleItem.style.backgroundImage = 'url(images/circlepatternrole.svg)'
     let roleName = "teacher"
     let roleSrc = "teacher"
 
@@ -37,17 +37,22 @@ localUser.role.forEach(role => {
 
     roleItem.innerHTML = `
         <section class="select-role__info">
-            <div class="select-role__icon">
+            <div class="select-role__icon${role !== 'teacher' ? ' select-role__icon--special' : ''}">
                 <img src="./images/${roleSrc}roleicon.svg" alt="">
             </div>
             <h5 class="select-role__title">${roleName}</h5>
         </section>
         <section class="select-role__controls">
-            <button class="small-button small-button--secondary">
+            <button class="small-button small-button--secondary selectRoleButton">
                 <span>Seleccionar</span>
             </button>
         </section>
     `
-
     selectRoleContainer.appendChild(roleItem)
+
+    const selectRoleButton = roleItem.querySelector(".selectRoleButton")
+    selectRoleButton.addEventListener('click', () => {
+        setLocalStorage('currentRole', role)
+        window.location = "index.html"
+    })
 });
--- scripts/utils/ls.js
+++ scripts/utils/ls.js
@@ -2,7 +2,7 @@ export const ls = window.localStorage;
 export const localUser = JSON.parse(ls.getItem('currentuser'))
 export const localSubjects = JSON.parse(ls.getItem('subjectList'))
 export const localPeriod = JSON.parse(ls.getItem('currentPeriod'))
-export const localRole = JSON.parse(ls.getItem('currentRole'))
+export const localRole = ls.getItem('currentRole')
 
 export function setLocalStorage(tag, value) {
     ls.setItem(tag, value)
